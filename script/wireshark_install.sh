540ce4c3f7384a2365959b57f622e111b632bfb3
Laurent LEQUIEVRE <laurent.lequievre@univ-bpclermont.fr>
2017-06-29 18:56:57 +0200

    add wireshark script

---
+++ script/wireshark_install.sh
@@ -0,0 +1,9 @@
+sudo apt-get install wireshark
+sudo dpkg-reconfigure wireshark-common
+sudo groupadd wireshark
+sudo usermod -a -G wireshark $USER
+sudo chgrp wireshark /usr/bin/dumpcap
+sudo chmod 750 /usr/bin/dumpcap
+sudo setcap cap_net_raw,cap_net_admin=eip /usr/bin/dumpcap
+sudo getcap /usr/bin/dumpcap
+sudo reboot
